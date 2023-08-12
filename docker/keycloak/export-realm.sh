3b6d56b1ac1660f983bd59dd5464a51139a2a114
Paweł Cembaluk <pawel.cembaluk@gmail.com>
2023-08-12 19:34:53 +0200

    Add realm export/import to Keycloak.

---
+++ docker/keycloak/export-realm.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+CONTAINER_NAME=office_buddy_keycloak
+REALM_NAME=office-buddy
+
+docker exec -it "${CONTAINER_NAME}" /opt/keycloak/bin/kc.sh export \
+  --file=/tmp/"${REALM_NAME}"-realm.json \
+  --realm "${REALM_NAME}" \
+  --users realm_file
+
+docker cp "${CONTAINER_NAME}":/tmp/"${REALM_NAME}"-realm.json ./
