2c61afea77e5727a24782db045455c913f81b264
Paweł Cembaluk <pawel.cembaluk@gmail.com>
2023-08-12 19:37:53 +0200

    Minor script style adjustments.

--- docker/keycloak/export-realm.sh
+++ docker/keycloak/export-realm.sh
@@ -3,9 +3,9 @@
 CONTAINER_NAME=office_buddy_keycloak
 REALM_NAME=office-buddy
 
-docker exec -it "${CONTAINER_NAME}" /opt/keycloak/bin/kc.sh export \
-  --file=/tmp/"${REALM_NAME}"-realm.json \
-  --realm "${REALM_NAME}" \
+docker exec -it "$CONTAINER_NAME" /opt/keycloak/bin/kc.sh export \
+  --file=/tmp/"$REALM_NAME"-realm.json \
+  --realm "$REALM_NAME" \
   --users realm_file
 
-docker cp "${CONTAINER_NAME}":/tmp/"${REALM_NAME}"-realm.json ./
+docker cp "$CONTAINER_NAME":/tmp/"$REALM_NAME"-realm.json ./
