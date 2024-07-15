da6849b06ab1b0a2fbbfe684f61fd952693820c4
Paweł Cembaluk <pawel.cembaluk@gmail.com>
2024-07-15 21:00:20 +0200

    Implement KeycloakRolesExtractorSuite.

--- src/main/scala/adapters/keycloak/auth/service/KeycloakRolesExtractor.scala
+++ src/main/scala/adapters/keycloak/auth/service/KeycloakRolesExtractor.scala
@@ -6,10 +6,10 @@ import adapters.keycloak.repository.account.KeycloakRole
 import domain.model.account.Role
 import io.circe.Json
 
-class KeycloakRolesExtractor extends RolesExtractor {
+object KeycloakRolesExtractor extends RolesExtractor {
 
   override def extract(json: Json): List[Role] =
-    json.hcursor // TODO: Verify if this logic is correct for Keycloak JWTs (via IT test?)
+    json.hcursor
       .downField("realm_access")
       .downField("roles")
       .as[List[String]]
