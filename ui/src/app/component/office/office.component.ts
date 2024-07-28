f677fe99663fdf445bc6f5f3b39f45bb3e18bdca
Paweł Cembaluk <pawel.cembaluk@gmail.com>
2024-07-28 21:19:13 +0200

    WIP: Add office card.

--- ui/src/app/component/office/office.component.ts
+++ ui/src/app/component/office/office.component.ts
@@ -7,4 +7,61 @@ import { Component } from '@angular/core';
 })
 export class OfficeComponent {
 
+  offices = [
+    {
+      name: "Wrocław Office",
+      address: {
+        addressLine1: "ul. Powstańców Śląskich 9",
+        addressLine2: "1st floor",
+        postalCode: "53-332",
+        city: "Wrocław",
+        country: "Poland"
+      },
+      officeManagers: [
+        "John Doe",
+        "Jane Doe"
+      ],
+      assignedAccounts: 123,
+      desks: 9,
+      parkingSpots: 2,
+      rooms: 6,
+      activeReservations: 42
+    },
+    {
+      name: "Kraków Office",
+      address: {
+        addressLine1: "ul. Krzywa 12",
+        addressLine2: null,
+        postalCode: "12-443",
+        city: "Kraków",
+        country: "Poland"
+      },
+      officeManagers: [
+        "Sue Smith"
+      ],
+      assignedAccounts: 42,
+      desks: 30,
+      parkingSpots: 9,
+      rooms: 3,
+      activeReservations: 5
+    },
+    {
+      name: "Warsaw Office",
+      address: {
+        addressLine1: "Plac Bankowy 2",
+        addressLine2: "5th floor",
+        postalCode: "99-100",
+        city: "Warsaw",
+        country: "Poland"
+      },
+      officeManagers: [
+        "James Brown"
+      ],
+      assignedAccounts: 12,
+      desks: 3,
+      parkingSpots: 1,
+      rooms: 1,
+      activeReservations: 1
+    }
+  ]
 }
