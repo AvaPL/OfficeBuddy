df2be293cdd7510f59e747331fd6aea3c3e0b04d
Paweł Cembaluk <pawel.cembaluk@gmail.com>
2024-07-30 21:05:45 +0200

    Add scrolling to the offices list.

--- ui/src/app/component/office/office.component.ts
+++ ui/src/app/component/office/office.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-office',
@@ -65,6 +65,139 @@ export class OfficeComponent {
       parkingSpots: 1,
       rooms: 1,
       activeReservations: 1
+    },
+    {
+      id: "4",
+      name: "London Office",
+      address: {
+        addressLine1: "123 Baker Street",
+        addressLine2: null,
+        postalCode: "W1U 6TY",
+        city: "London",
+        country: "United Kingdom"
+      },
+      officeManagers: [
+        "Sherlock Holmes"
+      ],
+      assignedAccounts: 1,
+      desks: 1,
+      parkingSpots: 0,
+      rooms: 1,
+      activeReservations: 0
+    },
+    {
+      id: "5",
+      name: "New York Office",
+      address: {
+        addressLine1: "123 Wall Street",
+        addressLine2: null,
+        postalCode: "10005",
+        city: "New York",
+        country: "United States"
+      },
+      officeManagers: [
+        "John Smith"
+      ],
+      assignedAccounts: 0,
+      desks: 0,
+      parkingSpots: 0,
+      rooms: 0,
+      activeReservations: 0
+    },
+    {
+      id: "6",
+      name: "San Francisco Office",
+      address: {
+        addressLine1: "123 Market Street",
+        addressLine2: null,
+        postalCode: "94105",
+        city: "San Francisco",
+        country: "United States"
+      },
+      officeManagers: [
+        "Jane Smith"
+      ],
+      assignedAccounts: 0,
+      desks: 0,
+      parkingSpots: 0,
+      rooms: 0,
+      activeReservations: 0
+    },
+    {
+      id: "7",
+      name: "Sydney Office",
+      address: {
+        addressLine1: "123 George Street",
+        addressLine2: null,
+        postalCode: "2000",
+        city: "Sydney",
+        country: "Australia"
+      },
+      officeManagers: [
+        "Alice Brown"
+      ],
+      assignedAccounts: 0,
+      desks: 0,
+      parkingSpots: 0,
+      rooms: 0,
+      activeReservations: 0
+    },
+    {
+      id: "8",
+      name: "Tokyo Office",
+      address: {
+        addressLine1: "123 Ginza Street",
+        addressLine2: null,
+        postalCode: "104-0061",
+        city: "Tokyo",
+        country: "Japan"
+      },
+      officeManagers: [
+        "Bob Johnson"
+      ],
+      assignedAccounts: 0,
+      desks: 0,
+      parkingSpots: 0,
+      rooms: 0,
+      activeReservations: 0
+    },
+    {
+      id: "9",
+      name: "Singapore Office",
+      address: {
+        addressLine1: "123 Orchard Road",
+        addressLine2: null,
+        postalCode: "238879",
+        city: "Singapore",
+        country: "Singapore"
+      },
+      officeManagers: [
+        "Eve White"
+      ],
+      assignedAccounts: 0,
+      desks: 0,
+      parkingSpots: 0,
+      rooms: 0,
+      activeReservations: 0
+    },
+    {
+      id: "10",
+      name: "Dubai Office",
+      address: {
+        addressLine1: "123 Sheikh Zayed Road",
+        addressLine2: null,
+        postalCode: "123456",
+        city: "Dubai",
+        country: "United Arab Emirates"
+      },
+      officeManagers: [
+        "Michael Black"
+      ],
+      assignedAccounts: 0,
+      desks: 0,
+      parkingSpots: 0,
+      rooms: 0,
+      activeReservations: 0
     }
   ]
 
